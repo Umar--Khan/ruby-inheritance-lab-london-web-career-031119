6b0d87f9bfe02bf1755bea0fd17a712dd0090fee
Umar Khan <umarkhandev@gmail.com>
2019-03-16 22:04:22 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < Teacher
 
+    attr_accessor :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn (string)
+        @knowledge << string
+    end
 end
